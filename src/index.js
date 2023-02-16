5b239e47155e723ea12325ef982157ee8695b8aa
Gabriel-025 <gabrielmachado210prata@gmail.com>
2023-02-16 13:14:52 -0300

    feat:added prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import _ from 'lodash';
-import './style.css'
+import _ from "lodash";
+import "./style.css";
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Welcome', 'to  webpack'], ' ');
+  element.innerHTML = _.join(["Welcome", "to  webpack"], " ");
 
-
-  element.classList.add('hello');
+  element.classList.add("hello");
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode:'development',
-    entry: {
-        index: './src/index.js',
-        print: './src/print.js',
-        style: './src/style.css'
-    },
-    devServer: {
-        static: './dist',
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+    style: "./src/style.css",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-    plugins: [
-        new HtmlWebpackPlugin({
-        title: 'Output Management',
-        }),
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    }, 
-    module: {
-        rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        ],
-    },
-    optimization: {
-        runtimeChunk: 'single',
-      },
-    };
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
